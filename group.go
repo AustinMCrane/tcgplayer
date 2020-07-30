4cfee968c724184a145e293ca2c2efaabe970de6
Austin Crane <austin.crane@accuweather.com>
2020-07-30 13:20:58 -0500

    added limit offset to ge groups

--- group.go
+++ group.go
@@ -1,14 +1,31 @@
 package tcgplayer
 
 import (
+	"fmt"
 	"strconv"
 
 	"github.com/pkg/errors"
 )
 
-func (client *Client) GetGroups(categoryID int) ([]*Group, error) {
+type GroupParams struct {
+	CategoryID int `json:"categoryId"`
+	Limit      int `json:"limit"`
+	Offset     int `json:"offset"`
+}
+
+func (client *Client) GetGroups(params GroupParams) ([]*Group, error) {
+	u := "/catalog/categories/" + strconv.Itoa(params.CategoryID) + "/groups"
+
+	if params.Limit != 0 {
+		u = u + "?limit=" + fmt.Sprintf("%d", params.Limit)
+	}
+
+	if params.Offset != 0 {
+		u = u + "&offset=" + fmt.Sprintf("%d", params.Offset)
+	}
+
 	var groupAPIResponse GroupAPIResponse
-	err := get(client, "/catalog/categories/"+strconv.Itoa(categoryID)+"/groups", nil, &groupAPIResponse)
+	err := get(client, u, nil, &groupAPIResponse)
 	if err != nil {
 		return nil, err
 	}
--- group_test.go
+++ group_test.go
@@ -7,10 +7,17 @@ import (
 )
 
 func TestGetGroups(t *testing.T) {
+
 	client, err := New(*publicKey, *privateKey)
 	require.NoError(t, err)
 
-	groups, err := client.GetGroups(CategoryYugioh)
+	params := GroupParams{
+		CategoryID: CategoryYugioh,
+		Limit:      10,
+		Offset:     10,
+	}
+
+	groups, err := client.GetGroups(params)
 	require.NoError(t, err)
 	require.True(t, len(groups) > 0)
 }
