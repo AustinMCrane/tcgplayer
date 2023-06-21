0dd236a5c144372c8bfef85b5f7940b51354a104
Austin Crane <me@austinmcrane.com>
2023-06-20 20:21:20 -0500

    feat(categories): added get catergories

---
+++ category.go
@@ -0,0 +1,30 @@
+package tcgplayer
+
+import (
+	"github.com/pkg/errors"
+)
+
+type Category struct {
+	ID         int    `json:"categoryId"`
+	Name       string `json:"displayName"`
+	ModifiedOn string `json:"modifiedOn"`
+}
+type CategoryAPIResponse struct {
+	Results []*Category `json:"results"`
+}
+
+func (client *Client) GetCategories() ([]*Category, error) {
+	u := "/catalog/categories"
+
+	var categoryAPIResponse CategoryAPIResponse
+	err := get(client, u, nil, &categoryAPIResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(categoryAPIResponse.Results) == 0 {
+		return nil, errors.New("did not find any categories")
+	}
+
+	return categoryAPIResponse.Results, nil
+}
